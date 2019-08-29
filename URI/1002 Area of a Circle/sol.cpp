01391fe3bc8531d476ff6ea47ad3271e0530f056
Leonardo Su <leonardo.su@ccc.ufcg.edu.br>
2019-08-28 22:08:59 -0300

    Add files via upload

---
+++ URI/1002 Area of a Circle/sol.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    double R,pi;
+    pi=3.14159;
+
+    scanf("%lf",&R);
+
+    printf("A=%.4lf\n",R*R*pi);
+ 
+    return 0;
+}
---
+++ URI/1002 Area of a Circle/sol.py
@@ -0,0 +1,6 @@
+r = float(input())
+pi = float(3.14159)
+
+ans = float(pi*(r*r))
+
+print("A={:.4f}".format(ans))
