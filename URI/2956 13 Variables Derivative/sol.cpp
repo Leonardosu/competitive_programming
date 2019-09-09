18c16200164c33cdf7b1ca49a4963687b8c569e3
LeonardoSu <leonardosu0@gmail.com>
2019-09-08 23:41:46 -0300

    :ant:

---
+++ URI/2956 13 Variables Derivative/sol.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+double h,l;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+	cin>>h>>l;
+	cout<<fixed<<setprecision(5);
+	cout<<"Concluimos que, dado o limite da entrada, a resposta seria:  y = f(x) = "<<(h*l)/2.0<<".\n";
+}
+
+/*
+
+
+2
+Palpites
+Roma
+Alfonso Cuaron
+Glenn Close
+Christian Bale
+
+Vencedores
+Green Book
+Alfonso Cuaron
+Olivia Colman
+Rami Malek
+
+Palpites
+A Forma da Agua
+Guilhermo del Toro
+Sally Hawkins
+Daniel DayLewis
+
+Vencedores
+A Forma da Agua
+Guilhermo del Toro
+Frances McDormand
+Gary Oldman
+
+*/
