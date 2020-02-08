6c93883420cb4c9ad90b2da72cc9fea4a596236b
Leonardo Su <leonardosu0@gmail.com>
2020-02-08 22:15:33 +0000

    Create 10407.cpp

---
+++ UVA/10407.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+vector<ll> v;
+ll qnt,x;
+ll ans;
+bool stop = false;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	
+	while(cin>>x)
+	{
+		if(x == 0)
+		{
+			if(stop) break;
+			stop = true;
+			ans = abs(v[1] - v[0]);
+			int n = v.size();
+			for(int i=0;i<n-1;++i)
+				ans = __gcd(abs(v[i+1] - v[i]),ans);
+			cout<<ans<<"\n";
+			v.clear();
+		}
+		else
+		{
+			v.push_back(x);
+			stop = false;			
+		}
+	}
+	return 0;
+}
