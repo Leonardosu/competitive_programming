3a9740d8f5085745f156e50eb86f3217bbaa2eb7
Leonardo Su <leonardosu0@gmail.com>
2020-02-15 06:15:24 -0300

    Create 568.cpp

---
+++ UVA/568.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+typedef long long int ll;
+ll n,ans,c,c2,c5,now;
+ll f[10010];
+
+ll poww(ll a,ll n)
+{
+	ll ans = 1;
+	while(n > 0)
+	{
+		if(n&1) ans = (ans*a)%10;
+		a = (a*a)%10;
+		n >>= 1;
+	}
+	return ans;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	
+	ans = 1LL;
+	f[0] = 1;
+	c2 = 0;
+	c5 = 0;
+	for(ll i=1;i<=10005;++i)
+	{
+		c = i;
+
+		while(c%2 == 0) c/=2,c2++;
+		while(c%5 == 0) c/=5,c5++;
+		ans*=c;
+		ans%=10;
+
+		if(c2 > c5)
+			now = (ans*poww(2,c2-c5))%10;
+		else
+			now = (ans*poww(5,c5-c2))%10;
+		f[i] = now;
+	}
+
+	while(cin>>n)
+	{
+		cout<<setw(5);
+		cout<<n<<" -> "<<f[n]<<"\n";	
+	}	
+}
