60de6e2845123b1edf765215bc6260729082e82b
Leonardo Su <leonardo.su@ccc.ufcg.edu.br>
2023-01-10 18:08:09 -0300

    Update d.cpp

--- Atcoder/ABC/122/d.cpp
+++ Atcoder/ABC/122/d.cpp
@@ -10,30 +10,45 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
+//    *AGC , *ACG , *GAC , A*GC , AG*C
+bool valid(int mask) { //ACGT = 1234
+    if(mask % 1000 == 123 || mask % 1000 == 132 || mask % 1000 == 312    // caso simples
+        || mask == 1132 || mask == 1232 || mask == 1332 || mask == 1432  // A*GC 
+        || mask == 1312 || mask == 1322 || mask == 1332 || mask == 1342) // AG*C
+            return false;
+    return true;
+}
+
+const int N = 100 + 7;
+const int limit = 10000;
+const int mod = 1e9 + 7;
+ll dp[N][limit];
+
 int main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
     int n;
     cin>>n;
 
-    ll dp[n+1][4]; memset(dp, 0, sizeof dp);    
-    ll ans[n+1];
+    dp[0][0] = 1; //dp[tamanho][ultimas 4 letras]
 
-    dp[1][0] = dp[1][1] = dp[1][2] = dp[1][3] = 1;
-    dp[2][0] = dp[2][1] = dp[2][2] = dp[2][3] = 4;
-    ans[0] = 0; 
-    ans[1] = 4; 
-    ans[2] = 16;
-
-    const int mod = 1e9 + 7;
-    for(int i=3;i<=n;++i) {
-    	dp[i][0] = ans[i-1];
-	    dp[i][1] = ans[i-1];
-	    dp[i][2] = ans[i-1] - dp[i-2][0] + dp[i-3][2];
-	    dp[i][3] = ans[i-1] - dp[i-2][0] - dp[i-2][2] - dp[i-3][0]*3;
-	    ans[i] = (dp[i][0] + dp[i][1] + dp[i][2] + dp[i][3])%mod;
+    const int mod = 1e9 + 7;    //ACGT = 1234
+    for(int i=0;i<n;++i) {
+        for(int mask=0;mask<limit;++mask) {
+            if(dp[i][mask] != 0) {
+                for(int letra=1;letra<=4;++letra) {
+                    int new_mask = (mask%1000) * 10 + letra;
+                    if(valid(new_mask)) {
+                        dp[i+1][new_mask] = (dp[i+1][new_mask] + dp[i][mask]) % mod;
+                    }
+                }
+            }
+        }
     }
 
-    cout<<ans[n]<<"\n";
+    ll ans = 0;
+    for(int i=0;i<limit;++i) 
+        ans += dp[n][i];
+
+    cout<<ans%mod<<"\n";
 }
