c0f0c64f2293269796565f2893c708880a1377be
Leonardo Su <leonardo.su@ccc.ufcg.edu.br>
2023-05-31 02:14:55 -0300

    Create Apples and Bananas.cpp

---
+++ CSES/Advanced Techniques/Apples and Bananas.cpp
@@ -0,0 +1,80 @@
+#include "bits/stdc++.h"
+#define f first
+#define s second
+#define endl '\n'
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(),x.end()
+#define present(c, x) (c.find(x) != c.end())
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+
+typedef complex<double> C;
+typedef vector<double> vd;
+void fft(vector<C>& a) {
+    int n = sz(a), L = 31 - __builtin_clz(n);
+    static vector<complex<long double>> R(2, 1);
+    static vector<C> rt(2, 1); // (^ 10% faster if double)
+    for (static int k = 2; k < n; k *= 2) {
+        R.resize(n); rt.resize(n);
+        auto x = polar(1.0L, acos(-1.0L) / k);
+        rep(i,k,2*k) rt[i] = R[i] = i&1 ? R[i/2] * x : R[i/2];
+    }
+    vi rev(n);
+    rep(i,0,n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+    rep(i,0,n) if (i < rev[i]) swap(a[i], a[rev[i]]);
+    for (int k = 1; k < n; k *= 2)
+        for (int i = 0; i < n; i += 2 * k) rep(j,0,k) {
+            C z = rt[j+k] * a[i+j+k]; // (25% faster if hand−rolled)
+            a[i + j + k] = a[i + j] - z;
+            a[i + j] += z;
+        }
+}
+
+vd conv(const vd& a, const vd& b) {
+    if (a.empty() || b.empty()) return {};
+    vd res(sz(a) + sz(b) - 1);
+    int L = 32 - __builtin_clz(sz(res)), n = 1 << L;
+    vector<C> in(n), out(n);    copy(all(a), begin(in));
+    rep(i,0,sz(b)) in[i].imag(b[i]);
+    fft(in);
+    for (C& x : in) x *= x;
+    rep(i,0,n) out[i] = in[-i & (n - 1)] - conj(in[i]);
+    fft(out);
+    rep(i,0,sz(res)) res[i] = imag(out[i]) / (4 * n);
+    return res;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    double timing = clock();
+    
+    int k, n, m;
+    cin>>k>>n>>m;
+
+    vector<double> p1(2*k), p2(2*k);
+    for(int i=0;i<n;++i) {
+        int x;
+        cin>>x;
+        p1[x]++;
+    }
+
+    for(int i=0;i<m;++i) {
+        int x;
+        cin>>x;
+        p2[x]++;
+    }
+
+    auto p3 = conv(p1, p2);
+    int q = sz(p3);
+    
+    for(int i=2;i<=2*k;++i) 
+        cout<<(i>=q ? 0 : llround(p3[i]))<<' ';   
+    cout<<endl;
+    //--------------------------
+    cerr<<"\n\nREAL TIME: "<<fixed<<setprecision(4)<<(clock()-timing)/CLOCKS_PER_SEC<<endl;
+}
